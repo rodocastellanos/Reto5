a494fe72fc03f488a872371ed9a682ab1f934631
rodocastellanos <111164028+rodocastellanos@users.noreply.github.com>
2022-08-13 13:57:36 -0400

    Add files via upload

---
+++ Reto5/src/main/java/com/mycompany/reto5/controlador/ControladorMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reto5.controlador;
+
+import com.mycompany.reto5.vista.frmPrincipal;
+
+/**
+ *
+ * @author rcastellanos
+ */
+public class ControladorMain {
+    public static void main(String[] args) {
+    
+        new frmPrincipal().setVisible(true);
+        
+    }
+    
+}
